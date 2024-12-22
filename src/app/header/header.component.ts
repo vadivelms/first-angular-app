e9869e359fbca24f9d2190ffef443b64c34ce338
vadivelms <vadivelmso@gmail.com>
2024-12-22 20:41:18 +0530

    Managing states

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-header',
     standalone: true,
-    templateUrl: './app.header.component.html',
-    styleUrl: './app.header.component.css',
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
    
 })
 
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+  onSelectedUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
+}
