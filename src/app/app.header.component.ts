5f5c6084555fe373ffc9fc1b6c5e4cab470f4a2c
vadivelms <vadivelmso@gmail.com>
2024-12-22 17:48:35 +0530

    first componant - header

---
+++ src/app/app.header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './app.header.component.html',
+    styleUrl: './app.header.component.css',
+   
+})
+
+
+export class HeaderComponent { }
